b32ac515ea3d1357b2302dca7769f02be8a86030
Cezar2231 <sisko.10@abv.bg>
2024-05-12 23:58:13 +0300

    added shopping cart features

--- index.js
+++ index.js
@@ -1,151 +1,151 @@
-let openShopping = document.querySelector('.shopping');
-let closeShopping = document.querySelector('.closeShopping');
-let list = document.querySelector('.list');
-let listCard = document.querySelector('.listCard');
+let listProductHTML = document.querySelector('.listProduct');
+let listCartHTML = document.querySelector('.listCart');
+let iconCart = document.querySelector('.icon-cart');
+let iconCartSpan = document.querySelector('.icon-cart span');
 let body = document.querySelector('body');
-let total = document.querySelector('.total');
-let quantity = document.querySelector('.quantity');
+let closeCart = document.querySelector('.close');
+let checkOut = document.querySelector('.checkOut');
+let products = [];
+let cart = [];
 
-openShopping.addEventListener('click', ()=>{
-    body.classList.add('active');
+
+iconCart.addEventListener('click', () => {
+    body.classList.toggle('showCart');
 })
-closeShopping.addEventListener('click', ()=>{
-    body.classList.remove('active');
+closeCart.addEventListener('click', () => {
+    body.classList.toggle('showCart');
 })
 
-let products = [
-    {
-        id: 1,
-        name: 'Слушалки Plantronics Audio 628 USB',
-        image: 'el.jpg',
-        price: 60,
-        availableCount: 3
-    },
-    {
-        id: 2,
-        name: 'Слушалки Plantronics Audio 628 USB',
-        image: 'el.jpg',
-        price: 60,
-        availableCount: 1
-    },
-    {
-        id: 3,
-        name: 'Слушалки Plantronics Audio 628 USB',
-        image: 'el.jpg',
-        price: 50,
-        availableCount: 1
-    },
-    {
-        id: 4,
-        name: 'Слушалки Plantronics Audio 628 USB',
-        image: 'el.jpg',
-        price: 45,
-        availableCount: 1
-    },
-    {
-        id: 5,
-        name: 'Слушалки Plantronics Audio 628 USB',
-        image: 'el.jpg',
-        price: 38,
-        availableCount: 1
-    },
-    {
-        id: 6,
-        name: 'Слушалки Plantronics Audio 628 USB',
-        image: 'el.jpg',
-        price: 100,
-        availableCount: 1
+    const addDataToHTML = () => {
+
+        if(products.length > 0) 
+        {
+            products.forEach(product => {
+                let newProduct = document.createElement('div');
+                newProduct.dataset.id = product.id;
+                newProduct.classList.add('item');
+                newProduct.innerHTML = 
+                `<img src="${product.image}" alt="">
+                <div class="title text-start ms-3">${product.name}</div>
+                <hr class="m-1">
+                <div class="price text-start ms-3">${product.price} лв.</div>
+                <button class="addCart btn">Добави в количката</button>`;
+                listProductHTML.appendChild(newProduct);
+            });
+        }
     }
-];
-let listCards  = [];
-function initApp(){
-    products.forEach((value, key) =>{
-        let newDiv = document.createElement('div');
-        newDiv.classList.add('item');
-        newDiv.innerHTML = `
-            <img src="images/${value.image}">
-            <div class="title text-start ms-3">${value.name}</div>
-            <hr>
-            <div class="price text-start ms-3"><p class="card-text">${value.price.toLocaleString()}лв. Оставащо количество: <span class="available-count">${value.availableCount.toLocaleString()}</span></p></div>
-            <button id="addButton_${key}" class="btn" onclick="addToCard(${key})">Добави в количката</button>`;
-        list.appendChild(newDiv);
+
+    listProductHTML.addEventListener('click', (event) => {
+        let positionClick = event.target;
+        if(positionClick.classList.contains('addCart')){
+            let id_product = positionClick.parentElement.dataset.id;
+            addToCart(id_product);
+        }
+    })
+const addToCart = (product_id) => {
+    let positionThisProductInCart = cart.findIndex((value) => value.product_id == product_id);
+    if(cart.length <= 0){
+        cart = [{
+            product_id: product_id,
+            quantity: 1
+        }];
+    }else if(positionThisProductInCart < 0){
+        cart.push({
+            product_id: product_id,
+            quantity: 1
+        });
+    }else{
+        cart[positionThisProductInCart].quantity = cart[positionThisProductInCart].quantity + 1;
+    }
+    addCartToHTML();
+    addCartToMemory();
+}
+const addCartToMemory = () => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+const addCartToHTML = () => {
+    listCartHTML.innerHTML = '';
+    let totalQuantity = 0;
+    let totalPrice = 0;
+    if(cart.length > 0){
+        cart.forEach(item => {
+            totalQuantity = totalQuantity +  item.quantity;
+            let newItem = document.createElement('div');
+            newItem.classList.add('item');
+            newItem.dataset.id = item.product_id;
+
+            let positionProduct = products.findIndex((value) => value.id == item.product_id);
+            let info = products[positionProduct];
+            listCartHTML.appendChild(newItem);
+            const itemPrice = info.price * item.quantity;
+            totalPrice += itemPrice;
+            newItem.innerHTML = `
+            <div class="image">
+                    <img src="${info.image}">
+                </div>
+                <div class="name">
+                ${info.name}
+                </div>
+                <div class="totalPrice">${info.price * item.quantity} лв.</div>
+                <div class="quantity">
+                    <span class="minus"><</span>
+                    <span>${item.quantity}</span>
+                    <span class="plus">></span>
+                </div>
+            `;
+        })
+    }
+    iconCartSpan.innerText = totalQuantity;
+    checkOut.textContent = `Поръчай: ${totalPrice} лв.`;
+}
+
+listCartHTML.addEventListener('click', (event) => {
+    let positionClick = event.target;
+    if(positionClick.classList.contains('minus') || positionClick.classList.contains('plus')){
+        let product_id = positionClick.parentElement.parentElement.dataset.id;
+        let type = 'minus';
+        if(positionClick.classList.contains('plus')){
+            type = 'plus';
+        }
+        changeQuantityCart(product_id, type);
+    }
+})
+const changeQuantityCart = (product_id, type) => {
+    let positionItemInCart = cart.findIndex((value) => value.product_id == product_id);
+    if(positionItemInCart >= 0){
+        let info = cart[positionItemInCart];
+        switch (type) {
+            case 'plus':
+                cart[positionItemInCart].quantity = cart[positionItemInCart].quantity + 1;
+                break;
+        
+            default:
+                let changeQuantity = cart[positionItemInCart].quantity - 1;
+                if (changeQuantity > 0) {
+                    cart[positionItemInCart].quantity = changeQuantity;
+                }else{
+                    cart.splice(positionItemInCart, 1);
+                }
+                break;
+        }
+    }
+    addCartToHTML();
+    addCartToMemory();
+}
+
+const initApp = () => {
+    // Get data product
+    fetch('products.json')
+    .then(response => response.json())
+    .then(data => {
+        products = data;
+        addDataToHTML();
+
+        // Get data cart from memory
+        if(localStorage.getItem('cart')){
+            cart = JSON.parse(localStorage.getItem('cart'));
+            addCartToHTML();
+        }
     })
 }
 initApp();
-function addToCard(key){
-    if(products[key].availableCount > 0){
-        if(listCards[key] == null){
-            // copy product form list to list card
-            listCards[key] = JSON.parse(JSON.stringify(products[key]));
-            listCards[key].quantity = 1;
-            let addButton = document.getElementById(`addButton_${key}`);
-            if (addButton) {
-                addButton.textContent = 'Добавено в количката';
-            }
-            products[key].availableCount--; 
-        } else {
-            listCards[key].quantity++;
-            products[key].availableCount--; 
-        }
-        reloadCard();
-        // Update the available count in the UI
-        let availableCountElement = document.querySelector(`#addButton_${key}`).previousElementSibling.querySelector('.available-count');
-        if (availableCountElement) {
-            availableCountElement.textContent = products[key].availableCount;
-        }
-    } else {
-        alert("Продуктът вече не е в наличност.");
-    }
-}
-
-
-function reloadCard(){
-    listCard.innerHTML = '';
-    let count = 0;
-    let totalPrice = 0;
-    listCards.forEach((value, key)=>{
-        totalPrice = totalPrice + value.price;
-        count = count + value.quantity;
-        if(value != null){
-            let newDiv = document.createElement('li');
-            newDiv.style.zIndex = '999';
-            newDiv.innerHTML = `
-                <div><img src="images/${value.image}"/></div>
-                <div class="sm-words">${value.name}</div>
-                <div>${value.price.toLocaleString()}лв.</div>
-                <div>
-                    <button class="px-2 " onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
-                    <div class="count">${value.quantity}</div>
-                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
-                </div>`;
-                listCard.appendChild(newDiv);
-        }
-    })
-    total.innerText = `${totalPrice.toLocaleString()} лв.`;
-    quantity.innerText = count;
-}
-
-
-function changeQuantity(key, quantity){
-    if(quantity == 0){
-        delete listCards[key];
-        let addButton = document.getElementById(`addButton_${key}`);
-        if (addButton) {
-            addButton.textContent = 'Добави в количката';
-            products[key].availableCount++;
-        }
-    } else if (quantity <= products[key].availableCount) {
-        listCards[key].quantity = quantity;
-        listCards[key].price = quantity * products[key].price;
-    } else {
-        alert('Надвишава наличното количество.');
-        return; // Exit the function
-    }
-    reloadCard();
-
-    // Update the available count in the UI
-    let availableCountElement = document.querySelector(`#addButton_${key}`).previousElementSibling.querySelector('.available-count');
-    if (availableCountElement) {
-        availableCountElement.textContent = products[key].availableCount;
-    }
-}
