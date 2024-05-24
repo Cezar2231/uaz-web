91746c9bccf8f1a73676aaa82b873c09ef644f6e
Cezar2231 <sisko.10@abv.bg>
2024-05-24 15:53:44 +0300

    fixed modal possition

--- checkout.js
+++ checkout.js
@@ -73,59 +73,65 @@ addCartToHTML();
 document.getElementById('order-form').addEventListener('submit', function (event) {
     event.preventDefault(); 
 
-    // Get all required fields
-    let requiredFields = document.querySelectorAll('#order-form [required]');
-    let allValid = true;
+    // Check if the cart is not empty
+    if (listCart.length > 0) {
+        // Get all required fields
+        let requiredFields = document.querySelectorAll('#order-form [required]');
+        let allValid = true;
 
-    // Check if all required fields are filled out
-    requiredFields.forEach(field => {
-        if (!field.value) {
-            allValid = false;
-            field.classList.add('error');  
+        // Check if all required fields are filled out
+        requiredFields.forEach(field => {
+            if (!field.value) {
+                allValid = false;
+                field.classList.add('error');  
+            } else {
+                field.classList.remove('error'); 
+            }
+        });
+
+        // If all fields are valid, proceed with the form submission
+        if (allValid) {
+            let name = document.getElementById('name').value;
+            let phone = document.getElementById('phone').value;
+            let address = document.getElementById('address').value;
+            let message = document.getElementById('message').value;
+            let office = document.getElementById('office').value;
+            let city = document.getElementById('city').value;
+
+            let body = '<strong>Contact Information:</strong><br/> Имена: ' + name + '<br/> Телефон: ' + phone + '<br/> Адрес на доставка: ' +
+                address + '<br/> Съобщение: ' + message + '<br/> Офис: ' + office + '<br/> Град: ' + city;
+
+            body += '<br/><br/><strong>Поръчка:</strong>' + cartBody;
+
+            Email.send({
+                SecureToken : "7aef9683-a83b-4269-a214-0025887c76a8",
+                To : 'sisko.10@abv.bg',
+                From : 'sisko.10@abv.bg',
+                Subject : "Нова поръчка",
+                Body : body
+            }).then(
+                message => alert(message)
+            );
+
+            // Clear the input fields
+            document.getElementById('name').value = '';
+            document.getElementById('phone').value = '';
+            document.getElementById('address').value = '';
+            document.getElementById('message').value = '';
+            document.getElementById('office').value = '';
+            document.getElementById('city').value = '';
+
+            // Clear the cart
+            listCart = [];
+            updateCartCookie();
+            addCartToHTML();
+
+            alert('Поръчката ви е приета');
+            window.location.href = "index.html#thank-you-modal";
         } else {
-            field.classList.remove('error'); 
+            alert('Моля, попълнете всички задължителни полета.');
         }
-    });
-
-    // If all fields are valid, proceed with the form submission
-    if (allValid) {
-        let name = document.getElementById('name').value;
-        let phone = document.getElementById('phone').value;
-        let address = document.getElementById('address').value;
-        let message = document.getElementById('message').value;
-        let office = document.getElementById('office').value;
-        let city = document.getElementById('city').value;
-
-        let body = '<strong>Contact Information:</strong><br/> Имена: ' + name + '<br/> Телефон: ' + phone + '<br/> Адрес на доставка: ' +
-            address + '<br/> Съобщение: ' + message + '<br/> Офис: ' + office + '<br/> Град: ' + city;
-
-        body += '<br/><br/><strong>Поръчка:</strong>' + cartBody;
-
-        Email.send({
-            SecureToken : "7aef9683-a83b-4269-a214-0025887c76a8",
-            To : 'sisko.10@abv.bg',
-            From : 'sisko.10@abv.bg',
-            Subject : "Нова поръчка",
-            Body : body
-        }).then(
-            message => alert(message)
-        );
-
-        // Clear the input fields
-        document.getElementById('name').value = '';
-        document.getElementById('phone').value = '';
-        document.getElementById('address').value = '';
-        document.getElementById('message').value = '';
-        document.getElementById('office').value = '';
-        document.getElementById('city').value = '';
-
-        // Clear the cart
-        listCart = [];
-        updateCartCookie();
-        addCartToHTML();
-
-        alert('Поръчката ви е приета');
     } else {
-        alert('Моля, попълнете всички задължителни полета.');
+        alert('Моля, добавете продукт в количката преди да направите поръчка.');
     }
 });
